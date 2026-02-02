a446587c536765cd62f258a2d1e7eebbbb91b78e
Aakash Tandukar <me.aakash.tandukar@gmail.com>
2026-02-02 18:00:31 -0500

    Initial Commit

---
+++ ChaseWeather/ChaseWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaseWeatherApp.swift
+//  ChaseWeather
+//
+//  Created by aakash tandukar on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ChaseWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
